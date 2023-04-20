30a2e9b63028cc99e6e8acd14ace85a6067c4c01
Sebastian Echeverria <sebastian.echeverria@gmail.com>
2023-04-20 13:34:51 -0400

    Default output paths in example moved to data folder to be obtainable outside the container

--- test_woodbridge.sh
+++ test_woodbridge.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-bash run.sh test ./data/sat_data/woodbridge/ ./data/models/conv_03_13_18_1850.pth ./data/models/vgg16_model.pth -t params.txt
+bash run.sh test ./data/sat_data/woodbridge/ ./data/models/conv_03_13_18_1850.pth ./data/models/vgg16_model.pth -t ./data/params.txt
