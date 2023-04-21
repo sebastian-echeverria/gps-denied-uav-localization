ab337e85bb0fc1cd535ca25a662b5f630b2c97a0
Sebastian-echeverria <98341740+sei-secheverria@users.noreply.github.com>
2023-04-21 16:40:10 -0400

    Adding test scripts, adding annotation for backwards Python compatibility

---
+++ apply_woodbridge.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+bash run_container.sh run_local_apply.sh ./data/sat_data/woodbridge/images/m_4007430_ne_18_1_20100829.png ./data/sat_data/woodbridge/images/uav1.png ./data/models/conv_03_13_18_1850.pth
