73c82af3150d03819e9149c757cd7e29e9c15cd8
Sebastian-echeverria <98341740+sei-secheverria@users.noreply.github.com>
2023-05-04 19:05:07 -0400

    Avoid converting p to H twice.

--- apply.py
+++ apply.py
@@ -31,10 +31,10 @@ def calculate_homography_from_model(sat_image: Tensor, uav_image: Tensor, model_
     print("Done loading net.")
 
     print("Executing DLK net on both images to get motion parameters.")
-    p_lk, _ = dlk_net(sat_image, uav_image, tol=1e-4, max_itr=50, conv_flag=1)    
+    p_lk, homography = dlk_net(sat_image, uav_image, tol=1e-4, max_itr=50, conv_flag=1)    
     print("DLK execution ended.")
 
-    return p_lk
+    return p_lk, homography
 
 
 def main():
@@ -61,19 +61,17 @@ def main():
 
     # 3. Run the dlk_trained on these two images (it receives two batches, but in this case each batch will be of 1)
     # 4. Check out the params and homography matrix from dlk
-    p = calculate_homography_from_model(sat_image, uav_image, args.MODEL_PATH)
+    p, homography = calculate_homography_from_model(sat_image, uav_image, args.MODEL_PATH)
 
     # 5. Use matrix to apply it to one image, and somehow store the modified image to see results?
-    # Project image.
+    # Project image and save to file.
     projected_image, _ = image_processor.project_images(uav_image, p)
-
-    # Save to file.
     image_io.save_tensor_image_to_file(projected_image, "./data/projected.png")
 
     # 6. LATER: convert to GPS coordinates.
     # TODO: Need to have cropped GeoTIFF image with coordinates for this to work.
-    homography = image_processor.param_to_H(p)
-    #pix2coords.infer_coordinates_from_paths(args.PIC_PATH, args.SAT_PATH, homography)
+    #gps_coords, _, _ = pix2coords.infer_coordinates_from_paths(args.PIC_PATH, args.SAT_PATH, homography)
+    #print(gps_coords)
 
 
 # Entry hook.
